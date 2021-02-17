fe47a69d9fca7d20706965063af7c1dd47f42c3b
Liat Meadows <liatmeadows@gmail.com>
2021-02-17 19:46:50 +0100

    header changes

--- client/src/Header.js
+++ client/src/Header.js
@@ -2,13 +2,29 @@ import Logo from "./Logo";
 import ProfilePic from "./ProfilePic";
 import Menu from "./Menu";
 import { Link } from "react-router-dom";
-import { useState, useEffect } from "react";
+// import { useState, useEffect } from "react";
 
 export default function Header(props) {
     let mql = window.matchMedia("(max-width: 600px)");
-    // console.log("mql: ", mql);
-    // console.log("mql media: ", mql.media);
-    // console.log("mql onChange: ", mql.onChange);
+    console.log("mql: ", mql);
+    console.log("mql media: ", mql.media);
+    console.log("mql onChange: ", mql.onChange);
+
+    // const [mQuery, setMQuery] = useState();
+
+    // function updateSize() {
+    //     setMQuery([window.innerWidth, window.innerHeight]);
+    // }
+
+    // useEffect(() => {
+    //     window.addEventListener("resize", this.updateSize);
+    // });
+
+    //     toggleUploader() {
+    //     this.setState({
+    //         uploaderVisible: !this.state.uploaderVisible,
+    //     });
+    // }
 
     // mediaQueryListEvent = () => {
     //     isTrusted: true,
@@ -28,13 +44,14 @@ export default function Header(props) {
     //     return () => mediaQuery.removeListener(setMQuery);
     // }, []);
 
+    /// WORKING VERSION
     return (
         <header>
             <Link className="menu-right" to="/">
                 <Logo />
             </Link>
             <div className="menu-right">
-                {mql.media ? (
+                {mql.matches ? (
                     <img className="icon" src="/images/burger.svg" />
                 ) : (
                     <Menu />
@@ -65,3 +82,6 @@ export default function Header(props) {
 // {mql ? <mobile /> : <Menu />}
 
 // <img onClick={toggleUploader} className="icon" src="/images/burger.svg" />;
+
+// onClick = { toggleUploader };
+// onChange={(e) => setMQuery(e.mql)}
