0831ab5404b113007fbb4d2f4ac824a260ae1a1d
Liat Meadows <liatmeadows@gmail.com>
2021-02-24 21:23:31 +0100

    social heroku

--- client/src/OtherProfile.js
+++ client/src/OtherProfile.js
@@ -1,5 +1,4 @@
 import { useEffect, useState } from "react";
-import { useSelector, useDispatch } from "react-redux";
 import axios from "./Axios";
 import { Link } from "react-router-dom";
 import FriendShipButton from "./FriendshipButton";
