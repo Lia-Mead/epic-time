8ac7505b6caf76ae9cf4718a2c7337633a068cb8
Liat Meadows <liatmeadows@gmail.com>
2021-02-25 01:55:59 +0100

    bio bug fix

--- client/src/App.js
+++ client/src/App.js
@@ -56,13 +56,19 @@ export default class App extends Component {
         });
     }
 
+    // sayHello() {
+    //     console.log("i am saying hello from app");
+    // }
+
     render() {
         // console.log("this.state in app: ", this.state);
         if (!this.state.id) {
-            // return null;
-            <div className="spinner-page">
-                <img src="/images/loading.svg" className="spinner" />
-            </div>;
+            return null;
+            // return (
+            //     <div className="spinner-container">
+            //         <div className="spinner"></div>
+            //     </div>
+            // );
         }
 
         return (
